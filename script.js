18f4f6d65c746e0cc805d6e37b47b54ab15f7726
Goran002 <146785439+Goran002@users.noreply.github.com>
2024-09-13 23:02:49 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+// Task 1 
 
+// let a = prompt('Enter the number: ')  ; 
+// let b = (a % 2 == 0) ? "even" : "odd" ; 
+// console.log(b) 
+
+    
+//Task 2 
+
+// let a = prompt('Enter the first number') , b = prompt('Enter the  second number');
+// let c = (a % b == 0 || b % a == 0 ) ?  1 : 0 
+// console.log(c) 
+
+//Task 3 
+
+// let a = +prompt(),b = +prompt();
+// console.log(180 - (a + b))
+
+//Task 4 
+
+// let a = +prompt() , b = +prompt() , c = +prompt() , d = +prompt()  , e = +prompt() ; 
+// console.log((a + b + c + d + e) / 5 )
+
+//Task 5 
+
+// let a = prompt() ; 
+// if(a % 3 == 0 && a % 5 == 0 && a % 7 == 0){ 
+//     console.log(a + " " + 'multiple of 3,5 and 7');
+// } else if(a % 3 == 0 && a % 5 == 0 ){ 
+//     console.log(a + ' ' + ' multiple of 3,5')
+// } else if(a % 3 == 0 && a % 7 == 0){ 
+//     console.log(a + ' ' + 'multiple of 3,7')
+// } else if(a % 5 == 0 && a % 7 == 0 ){ 
+//     console.log(a + ' ' + 'multiple of 5,7')
+// } else if(a % 3 == 0){ 
+//     console.log(a + ' ' + 'multiple of 3') 
+// }  else if(a % 5 == 0 ){ 
+//     console.log(a + ' ' + 'multiple of 5')
+// } else {
+//     console.log(a + ' ' + 'multiple of 7')
+// }
